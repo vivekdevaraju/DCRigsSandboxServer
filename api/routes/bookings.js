fb23a1425a7bdad0ca426310812ea75b1607c084
Ramchander <vivek.ramchander@accenture.com>
2018-04-23 12:24:10 +0530

    Updated booking methods

--- api/routes/bookings.js
+++ api/routes/bookings.js
@@ -1,5 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/',(req,res,next) =>{
+res.status(200).json({
+    message:'bookings were fetched'
+});
+});
 
+router.post('/',(req,res,next) =>{
+        res.status(202).json({
+        message:'bookings were created'
+        });
+});
+
+
+router.get('/:bookingId',(req,res,next) =>{
+    res.status(200).json({
+        message:'booking with Specific Id was fetched',
+        orderId : req.params.bookingId
+    });
+});
+
+router.delete('/:bookingId',(req,res,next) =>{
+    res.status(200).json({
+        message:'bookings were deleted',
+        orderId : req.params.bookingId
+    });
+});
 module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const productRoutes = require('./api/routes/products');
+const bookingRoutes = require('./api/routes/bookings');
+
 
 app.use('/products', productRoutes);
+app.use('/bookings', bookingRoutes);
+
 
 module.exports = app;
